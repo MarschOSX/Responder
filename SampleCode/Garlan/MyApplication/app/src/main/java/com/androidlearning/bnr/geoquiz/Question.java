a360be63aee479367076a706836bfbac44f82afa
gabowser <gabowser@gmail.com>
2015-09-28 11:49:08 -0400

    More Tutorial Work

---
+++ SampleCode/Garlan/MyApplication/app/src/main/java/com/androidlearning/bnr/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.androidlearning.bnr.geoquiz;
+
+/**
+ * Created by Garlan on 9/27/2015.
+ */
+public class Question {
+    private int mTextResID;
+    private boolean mAnswerTrue;
+
+    public Question(int textResID, boolean answerTrue){
+        this.mTextResID = textResID;
+        this.mAnswerTrue = answerTrue;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResID() {
+
+        return mTextResID;
+    }
+
+    public void setTextResID(int textResID) {
+        mTextResID = textResID;
+    }
+}
