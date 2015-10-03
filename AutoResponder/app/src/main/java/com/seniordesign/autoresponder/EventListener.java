f01f2294d2fa4365aa7abe259b5f63f910bffc77
MarschOSX <mdichman@ycp.edu>
2015-10-03 15:07:07 -0400

    Recieves and responds to an SMS!!!

--- AutoResponder/app/src/main/java/com/seniordesign/autoresponder/EventListener.java
+++ AutoResponder/app/src/main/java/com/seniordesign/autoresponder/EventListener.java
@@ -5,17 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsManager;
-import android.provider.Telephony;
+import android.telephony.*;
 import android.telephony.SmsMessage;
 
 public class EventListener extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        boolean checked = true;
-        android.util.Log.v("AutoResponder is ", java.lang.Boolean.toString(checked));
-
-        //Method 1
+        /*Method 1
         if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
             for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
                 android.util.Log.v("AutoResponder is ", java.lang.Boolean.toString(checked));
@@ -23,27 +20,29 @@ public class EventListener extends BroadcastReceiver{
                 SmsManager sms = SmsManager.getDefault();
                 sms.sendTextMessage("8568327320", null, messageBody, null, null);
             }
-        }
+        }*/
 
 
 
         //Method 2
-        android.util.Log.v("AutoResponder is ", java.lang.Boolean.toString(checked));
-        String message = "Test ";
-        Bundle bundle=intent.getExtras();
+       // if (intent.getAction().equals(android.telephony.SMS_RECEIVED)) {
+            android.util.Log.v("SMSForward,", "Intent received: " + intent.getAction());
+            String message = "Thunder";
+            Bundle bundle = intent.getExtras();
 
-        Object[] messages=(Object[])bundle.get("pdus");
-        if(messages != null) {
-            SmsMessage[] sms = new SmsMessage[messages.length];
-            for(int n=0;n<messages.length;n++){
-                sms[n]=SmsMessage.createFromPdu((byte[]) messages[n]);
-                message += sms[n];
+            Object[] messages = (Object[]) bundle.get("pdus");
+            if (messages != null) {
+                SmsMessage[] sms = new SmsMessage[messages.length];
+                for (int n = 0; n < messages.length; n++) {
+                    sms[n] = SmsMessage.createFromPdu((byte[]) messages[n]);
+                    message += sms[n];
+                }
             }
-        }
 
-        //Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
-        SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage("+18568327320", null, message, null, null);
+            //Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage("18568327320", null, message, null, null);
+       // }
     }
 
 }
--- AutoResponder/app/src/main/java/com/seniordesign/autoresponder/Main.java
+++ AutoResponder/app/src/main/java/com/seniordesign/autoresponder/Main.java
@@ -62,7 +62,7 @@ public class Main extends AppCompatActivity {
             case R.id.autoRespond_switch:
                     Log.v("AutoResponder Active? ", java.lang.Boolean.toString(checked));
                     SmsManager sms = SmsManager.getDefault();
-                    sms.sendTextMessage("+18568327320", null, "Thunder!", null, null);
+                    sms.sendTextMessage("18568327320", null, "Thunder!", null, null);
                 break;
         }
     }
