956d97a7bcccbdbf0969a39a80cac0adbf51bec1
MarschOSX <mdichman@ycp.edu>
2015-10-03 14:28:09 -0400

    attempting to respond to an SMS

--- AutoResponder/app/src/main/java/com/seniordesign/autoresponder/EventListener.java
+++ AutoResponder/app/src/main/java/com/seniordesign/autoresponder/EventListener.java
@@ -1,39 +1,54 @@
 package com.seniordesign.autoresponder;
 
+//import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+//import android.os.Bundle;
+//import android.telephony.SmsManager;
+import android.provider.Telephony;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
-import android.widget.Toast;
+//import android.util.*;
+//import android.widget.Toast;
 
 
 
 public class EventListener extends BroadcastReceiver{
-    @Override
-    public void onReceive(Context context, Intent intent)
+    //@Override
+    /*public void onReceive(Context context, Intent intent)
     {
+        boolean checked = true;
+        android.util.Log.v("AutoResponder is ", java.lang.Boolean.toString(checked));
+        String message = "Test ";
+        Bundle bundle=intent.getExtras();
 
-        Bundle extras = intent.getExtras();
-        String message = "";
-        if (extras != null)
-        {
-            Bundle bundle=intent.getExtras();
-
-            Object[] messages=(Object[])bundle.get("pdus");
-            if(messages != null) {
-                SmsMessage[] sms = new SmsMessage[messages.length];
-                for(int n=0;n<messages.length;n++){
-                    sms[n]=SmsMessage.createFromPdu((byte[]) messages[n]);
-                    message += sms[n];
-                }
+        Object[] messages=(Object[])bundle.get("pdus");
+        if(messages != null) {
+            SmsMessage[] sms = new SmsMessage[messages.length];
+            for(int n=0;n<messages.length;n++){
+                sms[n]=SmsMessage.createFromPdu((byte[]) messages[n]);
+                message += sms[n];
             }
-
-
-
         }
 
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage("8568327320", null, message, null, null);
+    }*/
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        boolean checked = true;
+        android.util.Log.v("AutoResponder is ", java.lang.Boolean.toString(checked));
+        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
+            for (SmsMessage smsMessage : Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+                android.util.Log.v("AutoResponder is ", java.lang.Boolean.toString(checked));
+                String messageBody = smsMessage.getMessageBody();
+                SmsManager sms = SmsManager.getDefault();
+                sms.sendTextMessage("8568327320", null, messageBody, null, null);
+            }
+        }
     }
 
 }
