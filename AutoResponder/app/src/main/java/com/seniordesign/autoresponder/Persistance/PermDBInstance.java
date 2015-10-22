f9134560f3b1324199c6d1282d322f44200b7a15
gabowser <gabowser@gmail.com>
2015-10-22 16:55:11 -0400

    Added comments to PermDBInstance

--- AutoResponder/app/src/main/java/com/seniordesign/autoresponder/Persistance/PermDBInstance.java
+++ AutoResponder/app/src/main/java/com/seniordesign/autoresponder/Persistance/PermDBInstance.java
@@ -21,14 +21,15 @@ import java.util.InputMismatchException;
 //note for setting up transactions: goto https://docs.oracle.com/javase/tutorial/jdbc/basics/transactions.html
 public class PermDBInstance implements DBInstance {
     private static final String TAG = "PermDBInstance";
-    private DBHelper myLittleHelper;
     private SQLiteDatabase myDB;
 
 
     public PermDBInstance(Context context) {
         //this.mDB = SQLiteDatabase.openOrCreateDatabase(DATABASE_NAME, null);
         Log.d(TAG, "initializing database instance");
-        this.myLittleHelper = new DBHelper(context);
+
+        //access DB via helper class
+        DBHelper myLittleHelper = new DBHelper(context);
         this.myDB = myLittleHelper.getWritableDatabase();
     }
 
@@ -37,17 +38,17 @@ public class PermDBInstance implements DBInstance {
     //SETTING TABLE FUNCTIONS//
     ///////////////////////////
     public void setReplyAll(String reply){
-        /*final String query =
-                "UPDATE " + DBHelper.TABLE_SETTINGS +
-                " SET " + DBHelper.COLUMN_VALUE[0] +" = " + reply +
-                " WHERE " + DBHelper.COLUMN_NAME[0] + " = " + Setting.REPLY_ALL;*/
         Log.d(TAG, "setting replyAll....");
         myDB.beginTransaction();
         try {
+            //set criteria for selecting row
             String filter = DBHelper.COLUMN_NAME[0] + "=" + "\"" + Setting.REPLY_ALL + "\"";
+
+            //set new value for column to be updated
             ContentValues args = new ContentValues();
             args.put(DBHelper.COLUMN_VALUE[0], reply);
 
+            //update column and check to make sure only 1 row was updated
             int updateNum =  myDB.update(DBHelper.TABLE_SETTINGS, args, filter, null);
             if (updateNum == 1) {
                 myDB.setTransactionSuccessful();
@@ -70,39 +71,39 @@ public class PermDBInstance implements DBInstance {
                 "SELECT " + DBHelper.COLUMN_VALUE[0] +
                  " FROM " + DBHelper.TABLE_SETTINGS +
                  " WHERE " + DBHelper.COLUMN_NAME[0] + "=" + "\"" + Setting.REPLY_ALL + "\"";
-        //SELECT value FROM  settings WHERE setting_name = "reply_all"
 
-        try {
-            Cursor result = myDB.rawQuery(query, null);
-            String response;
-            if ((result != null) && (result.moveToFirst())){
-                response = result.getString(result.getColumnIndex(DBHelper.COLUMN_VALUE[0]));
-                result.close();
-            }
-            else {
-                Log.e(TAG, "ERROR: " + getMethodName() + ": could not access cursor object from: " + query);
-                throw new NullPointerException();
-            }
+        //query db and ensure object was returned
+        Cursor result = myDB.rawQuery(query, null);
+        String response;
+        if ((result != null) && (result.moveToFirst())){ // move pointer to first row
+            //load setting value into string
+            response = result.getString(result.getColumnIndex(DBHelper.COLUMN_VALUE[0]));
+
+            //close the cursor
+            result.close();
+
+            //return the query result
             return response;
         }
-        catch (Exception e){
-            Log.e(TAG, "ERROR: " + getMethodName() + ": getReplyAll() failed");
-            throw e;
+        else {
+            Log.e(TAG, "ERROR: " + getMethodName() + ": could not access cursor object from: " + query);
+            return null;
         }
     }
 
     public void setDelay(int minutes){
-        /*final String query =
-                "UPDATE " + DBHelper.TABLE_SETTINGS +
-                " SET " + DBHelper.COLUMN_VALUE[0] +" = " + minutes +
-                " WHERE " + DBHelper.COLUMN_NAME[0] + " = " + Setting.TIME_DELAY;*/
 
         Log.d(TAG, "setting delay....");
         myDB.beginTransaction();
         try {
+            //set filter to determine row to select
             String filter = DBHelper.COLUMN_NAME[0] + "=" + Setting.TIME_DELAY;
+
+            //load values to be stored in respective columns
             ContentValues args = new ContentValues();
             args.put(DBHelper.COLUMN_VALUE[0], minutes);
+
+            //update row and ensure only 1 row was updated
             int updateNum = myDB.update(DBHelper.TABLE_SETTINGS, args, filter, null);
             if (updateNum == 1) {
                 myDB.setTransactionSuccessful();
@@ -127,25 +128,21 @@ public class PermDBInstance implements DBInstance {
                  " FROM " + DBHelper.TABLE_SETTINGS +
                  " WHERE " + DBHelper.COLUMN_NAME[0] + " = " + "\"" + Setting.TIME_DELAY + "\"";
 
-        try {
-            Cursor result = myDB.rawQuery(query, null);
-            int delay;
-            String value;
-            if ((result != null) && (result.moveToFirst())){
-                value = result.getString(result.getColumnIndex(DBHelper.COLUMN_VALUE[0]));
-                delay = Integer.parseInt(value);
-                result.close();
-            }
-            else {
-                Log.e(TAG, "ERROR: " + getMethodName() + ": could not access cursor object from: " + query);
-                throw new NullPointerException();
-            }
+        //query db and ensure cursor object returned is valid
+        Cursor result = myDB.rawQuery(query, null);
+        int delay;
+        String value;
+        if ((result != null) && (result.moveToFirst())){
+
+            //retrieve and return setting value
+            value = result.getString(result.getColumnIndex(DBHelper.COLUMN_VALUE[0]));
+            delay = Integer.parseInt(value);
+            result.close();
             return delay;
         }
-        catch (Exception e){
-            Log.e(TAG, "ERROR: " + getMethodName() + " failed");
-            myDB.endTransaction();
-            throw e;
+        else {
+            Log.e(TAG, "ERROR: " + getMethodName() + ": could not access cursor object from: " + query);
+            return -1;
         }
     }
 
@@ -192,34 +189,31 @@ public class PermDBInstance implements DBInstance {
                  " FROM " + DBHelper.TABLE_SETTINGS +
                  " WHERE " + DBHelper.COLUMN_NAME[0] + " = " + "\"" + Setting.RESPONSE_TOGGLE + "\"";
 
-        try {
-            Cursor result = myDB.rawQuery(query, null);
-            boolean toggle;
-            if ((result != null) && (result.moveToFirst())){
-                String response = result.getString(result.getColumnIndex(DBHelper.COLUMN_VALUE[0]));
-                //determine if value is true or false
-                if( response.compareTo("true") == 0){
-                    toggle = true;
-                }
-                else if ( response.compareTo("false") == 0){
-                    toggle = false;
-                }
-                else{
-                    Log.e(TAG, "ERROR: " + getMethodName() + ": found " + response + " when a value of true or false was expected from: " + query);
-                    throw new InputMismatchException();
-                }
-                result.close();
+        //query database and ensure cursor returned is valid
+        Cursor result = myDB.rawQuery(query, null);
+        boolean toggle;
+        if ((result != null) && (result.moveToFirst())){
+
+            //retrieve setting value
+            String response = result.getString(result.getColumnIndex(DBHelper.COLUMN_VALUE[0]));
+
+            //determine if value is true or false and returns as translates into a boolean
+            if( response.compareTo("true") == 0){
+                toggle = true;
             }
-            else {
-                Log.e(TAG, "ERROR: " + getMethodName() + ": could not get/access cursor object from: " + query);
-                throw new NullPointerException();
+            else if ( response.compareTo("false") == 0){
+                toggle = false;
             }
-            return toggle;
+            else{
+                Log.e(TAG, "ERROR: " + getMethodName() + ": found " + response + " when a value of true or false was expected from: " + query);
+                throw new InputMismatchException();
+            }
+            result.close();
+        } else {
+            Log.e(TAG, "ERROR: " + getMethodName() + ": could not get/access cursor object from: " + query);
+            throw new NullPointerException();
         }
-        catch (Exception e){
-            Log.e(TAG, "ERROR: " + getMethodName() + " failed");
-            throw e;
-        }
+        return toggle;
     }
 
     ////////////////////////////////
@@ -231,11 +225,12 @@ public class PermDBInstance implements DBInstance {
         try {
             //load columns in args
             ContentValues args = new ContentValues();
-            args.put(DBHelper.COLUMN_TIMESTAMP[0], newLog.getTimeStamp().toString());
+            args.put(DBHelper.COLUMN_TIMESTAMP[0], newLog.getTimeStamp().getTime());
             args.put(DBHelper.COLUMN_SENDERNUM[0], newLog.getSenderNumber());
             args.put(DBHelper.COLUMN_MESSAGERCV[0], newLog.getMessageReceived());
             args.put(DBHelper.COLUMN_MESSAGESNT[0], newLog.getMessageSent());
 
+            //add the row to the table and checks if insert was succesfull
             long insert = myDB.insertOrThrow(DBHelper.TABLE_RESPONSELOG, null, args);
             if (insert != -1) {
                 myDB.setTransactionSuccessful();
