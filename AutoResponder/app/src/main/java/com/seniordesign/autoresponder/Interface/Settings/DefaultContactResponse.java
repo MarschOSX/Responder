6643e54e9fccdfa69361be55906d837cd416244b
gabowser <gabowser@gmail.com>
2015-11-22 16:31:09 -0500

    Finished setting up configuration of sub-setting activities

--- AutoResponder/app/src/main/java/com/seniordesign/autoresponder/Interface/Settings/DefaultContactResponse.java
+++ AutoResponder/app/src/main/java/com/seniordesign/autoresponder/Interface/Settings/DefaultContactResponse.java
@@ -2,6 +2,8 @@ package com.seniordesign.autoresponder.Interface.Settings;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.seniordesign.autoresponder.R;
 
@@ -13,4 +15,25 @@ public class DefaultContactResponse extends ActionBarActivity {
         setContentView(R.layout.activity_default_contact_response);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_user_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
