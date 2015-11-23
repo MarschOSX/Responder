6e57c4fd31144e9b5c128d31ce2a2783924409c9
MarschOSX <mdichman@ycp.edu>
2015-11-23 12:33:48 -0500

    Basic Location and Activity Responses in EventHandler

--- AutoResponder/app/src/main/java/com/seniordesign/autoresponder/Receiver/EventHandler.java
+++ AutoResponder/app/src/main/java/com/seniordesign/autoresponder/Receiver/EventHandler.java
@@ -49,21 +49,42 @@ public class EventHandler {
                     updateLog.setTimeStamp(new Date(lastRecieved));
                     updateLog.setMessageReceived(message);
                     updateLog.setSenderNumber(phoneNumber);
-                    //get generalResponse from Database and set as message
+
+                    //get response from Database and set as message
                     String contactResponse = contact.getResponse();
                     String contactGroupName = contact.getGroupName();
                     Boolean contactInheritance = contact.isInheritance();
+                    String permissionsMessage;// = null
+
+
                     //Check the inheritance bit to see to use Contact Message or Group Message
                     if(contactInheritance){//inheritance from...
-                        if(!contactGroupName.matches(Group.DEFAULT_GROUP)) {//contact groups
+                        android.util.Log.v("EventHandler,", "Contact is inheriting group info/permissions");
+                        permissionsMessage = permissionsRequested(contact,message,true);
+                        if(permissionsMessage == null) {//no permissions, get normal response
+                            /*if (!contactGroupName.matches(Group.DEFAULT_GROUP)) {//contact groups
+                                message = db.getGroupInfo(contactGroupName).getResponse();
+                            } else {
+                                message = db.getReplyAll();
+                            }*/
                             message = db.getGroupInfo(contactGroupName).getResponse();
-                        }else{//default group
-                            message = db.getReplyAll();
+                            android.util.Log.v("EventHandler,", "Message is the group response");
+                        }else{
+                            message = permissionsMessage;
+                            android.util.Log.v("EventHandler,", "Message is an inherited location/activity response");
                         }
                     }else{//no inheritance, get contact info
-                        message = contactResponse;
-                    }
+                        android.util.Log.v("EventHandler,", "Contact is not inheriting, using its own info/permissions");
+                        permissionsMessage = permissionsRequested(contact,message,false);
+                        if(permissionsMessage == null) {
+                            message = contactResponse;
+                            android.util.Log.v("EventHandler,", "Message is the contact response");
+                        }else{
+                            message = permissionsMessage;
+                            android.util.Log.v("EventHandler,", "Message is a not inherited location/activity response");
 
+                        }
+                    }
                     updateLog.setMessageSent(message);
 
                     //Add number, message sent, message recieved, and lastRecieved time to DB
@@ -86,6 +107,49 @@ public class EventHandler {
         }
         android.util.Log.v("EventHandler,", "No Text Sent!");
         return -1;
+    }
+
+    public String permissionsRequested(Contact contact, String message, Boolean inheritance) {
+        Boolean locationPermission;
+        Boolean activityPermission;
+        String returnMessage;
+        if(inheritance) {
+            //we get permissions from the group
+            locationPermission = db.getGroupInfo(contact.getGroupName()).isLocationPermission();
+            activityPermission = db.getGroupInfo(contact.getGroupName()).isActivityPermission();
+        }else{
+            //we get permissions from the contact
+            locationPermission = contact.isLocationPermission();
+            activityPermission = contact.isActivityPermission();
+        }
+
+        if(!locationPermission && !activityPermission){//if both are false, nothing to do here
+            return null;
+        }
+
+
+        if (locationPermission) {//if Location permission is true
+            //you have permission, check SMS message to see if location was requested
+            if (message.contains("where are you")) {
+                //then we want to give that person your location
+                returnMessage = "I am at Kinsley.";//TODO make this not static
+                return returnMessage;
+            }
+            //TODO more examples here, maybe go to another class to check/compare SMS strings?
+            //TODO might want to return a drop pin instead of a message??
+        }
+
+        if (activityPermission) {//if Activity permission is true
+            //you have permission, check SMS message to see if activity was requested
+            if (message.contains("are you busy")) {
+                //then we want to give that person your calendar info
+                returnMessage = "I'm in class from 12pm-12am.";//TODO make this not static
+                return returnMessage;
+            }
+            //TODO more examples here, maybe go to another class to check/compare SMS strings?
+        }
+
+        return null;//if original message did not request location or activity
     }
 }
 
