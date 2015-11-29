f866a329cdd5aa8a6c550df50867e9786210984e
MarschOSX <mdichman@ycp.edu>
2015-11-29 14:40:20 -0500

    Working on getting cal info, EvHandler Restructure

--- AutoResponder/app/src/main/java/com/seniordesign/autoresponder/Receiver/EventHandler.java
+++ AutoResponder/app/src/main/java/com/seniordesign/autoresponder/Receiver/EventHandler.java
@@ -1,19 +1,23 @@
 package com.seniordesign.autoresponder.Receiver;
 
 
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.provider.CalendarContract;
 import android.telephony.SmsManager;
 
 import com.seniordesign.autoresponder.DataStructures.Contact;
 import com.seniordesign.autoresponder.DataStructures.Group;
 import com.seniordesign.autoresponder.DataStructures.ResponseLog;
 import com.seniordesign.autoresponder.Persistance.DBInstance;
+import com.seniordesign.autoresponder.R;
 
 import java.sql.Date;
 
 /*
  * Created by MarschOSX on 10/8/2015.
  */
-public class EventHandler {
+public class EventHandler extends ListActivity{
 
     private DBInstance db;
 
@@ -52,44 +56,23 @@ public class EventHandler {
 
                     //get response from Database and set as message
                     String contactResponse = contact.getResponse();
-                    String contactGroupName = contact.getGroupName();
-                    Boolean contactInheritance = contact.isInheritance();
-                    String permissionsMessage;// = null
+                    String permissionsMessage = permissionsRequested(contact,message);
 
-
-                    //Check the inheritance bit to see to use Contact Message or Group Message
-                    if(contactInheritance){//inheritance from...
-                        android.util.Log.v("EventHandler,", "Contact is inheriting group info/permissions");
-                        permissionsMessage = permissionsRequested(contact,message,true);
-                        if(permissionsMessage == null) {//no permissions, get normal response
-                            message = db.getGroupInfo(contactGroupName).getResponse();
-                            android.util.Log.v("EventHandler,", "Message is the group response");
-                        }else{
-                            message = permissionsMessage;
-                            android.util.Log.v("EventHandler,", "Message is an inherited location/activity response");
-                        }
-                    }else{//no inheritance, get contact info
-                        android.util.Log.v("EventHandler,", "Contact is not inheriting, using its own info/permissions");
-                        permissionsMessage = permissionsRequested(contact,message,false);
-                        if(permissionsMessage == null) {
-                            message = contactResponse;
-                            android.util.Log.v("EventHandler,", "Message is the contact response");
-                        }else{
-                            message = permissionsMessage;
-                            android.util.Log.v("EventHandler,", "Message is a not inherited location/activity response");
-
-                        }
+                    if(permissionsMessage == null) {//no permissions, get normal response
+                        message = contactResponse;
+                    }else{
+                        message = permissionsMessage;
                     }
-                    updateLog.setMessageSent(message);
-
-                    //Add number, message sent, message recieved, and lastRecieved time to DB
-                    db.addToResponseLog(updateLog);
-
                     //Send the GeneralResponse Message
                     SmsManager sms = SmsManager.getDefault();
 
                     android.util.Log.v("EventHandler,", "Message successfully sent to: " + phoneNumber + " Message Body: " + message);
                     sms.sendTextMessage(phoneNumber, null, message, null, null);
+
+                    //Add number, message sent, message recieved, and lastRecieved time to DB
+                    db.addToResponseLog(updateLog);
+                    updateLog.setMessageSent(message);
+
                     return 0;
                 } else {
                     android.util.Log.v("EventHandler,", "Cannot send a response yet!");
@@ -104,48 +87,91 @@ public class EventHandler {
         return -1;
     }
 
-    public String permissionsRequested(Contact contact, String message, Boolean inheritance) {
-        Boolean locationPermission;
-        Boolean activityPermission;
+    public String permissionsRequested(Contact contact, String message) {
         String returnMessage;
-        if(inheritance) {
-            //we get permissions from the group
-            locationPermission = db.getGroupInfo(contact.getGroupName()).isLocationPermission();
-            activityPermission = db.getGroupInfo(contact.getGroupName()).isActivityPermission();
-        }else{
-            //we get permissions from the contact
-            locationPermission = contact.isLocationPermission();
-            activityPermission = contact.isActivityPermission();
-        }
+
+        //TODO why do these functions go to group? Why is activity permission always true?
+        Boolean locationPermission = contact.isLocationPermission();
+        Boolean activityPermission = contact.isActivityPermission();
 
         if(!locationPermission && !activityPermission){//if both are false, nothing to do here
             return null;
+        }else if(locationPermission && activityPermission){//if they are both true
+            return null;//TODO how do we handle if both are requested
+        }else if (locationPermission) {//if just Location permission is true
+            returnMessage = getLocationInfo(message);
+            return returnMessage;
+            //TODO Why is activity permission always true?
+        }else if (activityPermission) {//if just Activity permission is true
+            returnMessage = getActivityInfo(message);
+            return returnMessage;
         }
-
-
-        if (locationPermission) {//if Location permission is true
-            //you have permission, check SMS message to see if location was requested
-            if (message.contains("where are you")) {
-                //then we want to give that person your location
-                returnMessage = "I am at Kinsley.";//TODO make this not static
-                return returnMessage;
-            }
-            //TODO more examples here, maybe go to another class to check/compare SMS strings?
-            //TODO might want to return a drop pin instead of a message??
-        }
-
-        if (activityPermission) {//if Activity permission is true
-            //you have permission, check SMS message to see if activity was requested
-            if (message.contains("are you busy")) {
-                //then we want to give that person your calendar info
-                returnMessage = "I'm in class from 12pm-12am.";//TODO make this not static
-                return returnMessage;
-            }
-            //TODO more examples here, maybe go to another class to check/compare SMS strings?
-        }
-
-        return null;//if original message did not request location or activity
+        return null;
     }
+
+
+    public String getActivityInfo(String message){
+        //you have permission, check SMS message to see if activity was requested
+        String[] possibleRequests = getResources().getStringArray(R.array.activity_requests_list);
+        String returnMessage = null;
+        //goes through a list of possible location responses
+        for (String possibleRequest : possibleRequests)
+            if (message.contains(possibleRequest)) {
+                //then we want to give that person your calendar info
+                try {//attempt to access calendar information
+                    long begin = System.currentTimeMillis() % 1000;
+                    //long end = begin;// ending time in milliseconds
+                            String[] query =
+                            new String[]{
+                                    CalendarContract.Instances._ID,
+                                    CalendarContract.Instances.BEGIN,
+                                    CalendarContract.Instances.END,
+                                    CalendarContract.Instances.EVENT_ID};
+                    Cursor cursor =
+                            CalendarContract.Instances.query(getContentResolver(), query, begin, begin);
+                    if (cursor.getCount() > 0) {//yes we are busy
+                        returnMessage = "I'm at an event right now";
+
+                        //get event ID so we can get more info on it
+                        /*int eventID = cursor.getColumnIndex(CalendarContract.Instances.EVENT_ID);
+                        String[] proj =
+                                new String[]{
+                                        CalendarContract.Events._ID,
+                                        CalendarContract.Events.DTSTART,
+                                        CalendarContract.Events.DTEND,
+                                        CalendarContract.Events.RRULE,
+                                        CalendarContract.Events.TITLE};
+                        Cursor cursor2 =
+                                getContentResolver().
+                                        query(CalendarContract.Events.CONTENT_URI, proj, CalendarContract.Events._ID + " = ? ", new String[]{Long.toString(eventID)}, null);
+                        if (cursor2.moveToFirst()) {
+                            // read event data
+                        }*/
+                    }
+
+                } catch (Exception e) {//if exception, just
+                    returnMessage = null;
+                }
+
+                return returnMessage;
+            }
+        return null;
+    }
+
+    public String getLocationInfo(String message){
+        String returnMessage;
+        //you have permission, check SMS message to see if location was requested
+        if (message.contains("where are you")) {
+            //then we want to give that person your location
+            returnMessage = "I am at Kinsley.";//TODO make this not static
+            return returnMessage;
+        }
+        //TODO more examples here, maybe go to another class to check/compare SMS strings?
+        //TODO might want to return a drop pin instead of a message??
+        return null;
+    }
+
+
 }
 
 
