77acf5074f769fff5a939f4b8612a17a36034ed2
Garlan <gabowser@gmail.com>
2016-03-22 11:50:54 -0400

    updated database documentation

--- AutoResponder/app/src/main/java/com/seniordesign/autoresponder/Persistance/DBInstance.java
+++ AutoResponder/app/src/main/java/com/seniordesign/autoresponder/Persistance/DBInstance.java
@@ -1,11 +1,8 @@
 package com.seniordesign.autoresponder.Persistance;
 
 import com.seniordesign.autoresponder.DataStructures.Contact;
-import com.seniordesign.autoresponder.DataStructures.DeveloperLog;
 import com.seniordesign.autoresponder.DataStructures.Group;
 import com.seniordesign.autoresponder.DataStructures.ResponseLog;
-
-import java.sql.Date;
 import java.util.ArrayList;
 
 /**
@@ -16,121 +13,134 @@ public interface DBInstance {
     ///////////////////////////
     //SETTING TABLE FUNCTIONS//
     ///////////////////////////
+    /** sets the default group response*/
     void setReplyAll(String reply);
 
+    /** @return the default group response */
     String getReplyAll();
 
+    /** sets the Universal Response*/
     void setUniversalReply(String universalReply);
 
+    /** @return the universal response*/
     String getUniversalReply();
 
+    /** set how long the responder will ignore a contact's messages after responding to a message*/
     void setDelay(int minutes);
 
+    /** @return time period in minutes before responding to the same phone number */
     int getDelay();
 
+    /** set how long AutoResponder will remain on until it disables itself */
     void setTimeLimit(int hours);
 
+    /** @return the time period in hours that the autoresponder is to be on*/
     int getTimeLimit();
 
+    /** record the time that the response toggle was set */
     void setTimeResponseToggleSet(long milliseconds);
 
+    /** @return when the response toggle was enabled */
     long getTimeResponseToggleSet();
 
+    /** @return the current period in minutes between getting the current location*/
     int getDrivingDetectionPeriod();
 
+    /** specify the time period in minutes between receipt of current location
+     * to determine impact on battery life*/
     void setDrivingDetectionPeriod(int minutes);
 
+    /** enable/disable all autoresponse functionality feature*/
     void setResponseToggle(boolean toggle);
 
+    /** enable/disable the Activity Response feature*/
     void setActivityToggle(boolean toggle);
 
+    /** enable/disable the Location Response feature*/
     void setLocationToggle(boolean toggle);
 
+    /** enable/disable the Universal Reply feature*/
     void setUniversalToggle(boolean toggle);
 
+    /** @return whether or not any messages can be responded to */
     boolean getResponseToggle();
-    
+
+    /** @return whether or not location requests can be responded to */
     boolean getLocationToggle();
 
+    /** @return whether or not activity requests can be responded to */
     boolean getActivityToggle();
 
+    /** @return the status of the Universal Reply Toggle*/
     boolean getUniversalToggle();
 
     ////////////////////////////////
     //RESPONSE LOG TABLE FUNCTIONS//
     ////////////////////////////////
 
+    /** adds new log to the response log*/
     void addToResponseLog(ResponseLog newLog);
 
+    /** @return the most recent response log for the specified phone number*/
     ResponseLog getLastResponseByNum(String phoneNum);
 
+    /** @return  the entire response log sorted from newest to oldest*/
     ArrayList<ResponseLog> getResponseLogList();
 
+    /** clears all entries from the response log table*/
     void deleteResponseLogs();
+
     ///////////////////////////
     //CONTACT TABLE FUNCTIONS//
     ///////////////////////////
 
-    /*adds a new contact
-    * @param Contact newContact
-    * @return # 0 for success,
-    * @return -1 if group does not exist*/
+    /**adds a new contact to the contact table
+    * @return # 0 for success, -1 if group does not exist*/
     int addContact(Contact newContact);
 
-    /*removes all contacts with that phone number
-    * @param String phoneNumber
+    /**removes all contacts with that phone number
     * @return # of contacts removed as int >= 0, or error code as int < 0*/
     int removeContact(String phoneNum);
 
-    /*changes a contacts name
-    * @param String phoneNumber, String newName
+    /**changes a contacts name
     * @return 0 for success, or error code as int < 0*/
     int setContactName(String phoneNum, String newName);
 
-    /*changes a contacts phoneNumber
-    * @param String oldPhoneNumber, String newPhoneNum
+    /**changes a contacts phoneNumber
     * @return 0 for success, or error code as int < 0*/
     int setContactNumber(String oldPhoneNum, String newPhoneNum);
 
-    /*changes a contacts locationPermission
-   * @param String phoneNum, boolean permission
+    /**changes a contacts locationPermission
    * @return 0 for success, or error code as int < 0*/
     int setContactResponse(String phoneNum, String response);
 
-    /*changes a contacts activityPermission
-   * @param String phoneNum, boolean permission
-   * @return 0 for success, or error code as int < 0*/
+    /**changes a contacts activityPermission
+      * @return 0 for success, or error code as int < 0*/
     int setContactLocationPermission(String phoneNum, boolean permission);
 
-    /*changes a contact's group name
-   * @param String phoneNum, String groupName
-   * @return # of rows updated, or error code as int < 0*/
+    /**changes a contact's group name
+      * @return # of rows updated, or error code as int < 0*/
     int setContactActivityPermission(String phoneNum, boolean permission);
 
-    /*changes a contact's inheritance, ie if true, when you run getContactInfo() it will
+    /**changes a contact's inheritance, ie if true, when you run getContactInfo() it will
        return a contact, but the response and permissions returned will be from the group
-   * @param String phoneNum, String groupName
-   * @return # of rows updated, or error code as int < 0*/
+      * @return # of rows updated, or error code as int < 0*/
     int setContactInheritance(String phoneNum, boolean inheritance);
 
-    /*changes a contact's response
-    * @param String phoneNum, String response
-    * @return # of rows updated,
-    * @return -1 if the new groupName does not exist*/
+    /**changes a contact's response
+        * @return # of rows updated, -1 if the new groupName does not exist*/
     int setContactGroup(String phoneNum, String groupName);
 
-    /*use to get contact info for a phonenumber
-   * @param String phoneNum
-   * @return contact if found, null if not found or there was an error*/
+    /**use to get contact info for a phonenumber
+      * @return contact if found, null if not found or there was an error*/
     Contact getContactInfo(String phoneNum);
 
-    /*returns sorted A - Z by name
+    /**returns sorted A - Z by name
    * @return contact if found, null if not found or there was an error*/
     ArrayList<Contact> getContactList();
 
-    /*returns list of contact 1 group sorted A - Z by name
-   * @param String phoneNum
-   * @return contact if found, null if not found or there was an error*/
+    /**returns list of contact 1 group sorted A - Z by name
+      * @return contact if found, null if not found or there was an error*/
     ArrayList<Contact> getGroup(String groupName);
 
     /////////////////////////
@@ -138,43 +148,36 @@ public interface DBInstance {
     /////////////////////////
 
     /*adds a new group
-    * @param Group newGroup
     * @return # 0 for success,
     * @return -1 if group already exists
     * */
     int addGroup(Group newGroup);
 
-    /*removes all groups with that name
-   * @param String phoneNumber
-   * @return # of contacts removed as int >= 0, or error code as int < 0*/
+    /**removes all groups with that name
+     * @return # of contacts removed as int >= 0, or error code as int < 0*/
     int removeGroup(String groupName);
 
-    /*changes a group's name
-    * @param String oldName, String newName
-    * @return 0 for success, or error code as int < 0*/
+    /**changes a group's name
+     * @return 0 for success, or error code as int < 0*/
     int changeGroupName(String oldName, String newName);
 
-    /*changes a group's location permission
-    * @param String String groupName, boolean permission
-    * @return 0 for success, or error code as int < 0*/
+    /**changes a group's location permission
+     * @return 0 for success, or error code as int < 0*/
     int setGroupResponse(String groupName, String response);
 
-    /*changes a group's activity permission
-     * @param String String groupName, boolean permission
+    /**changes a group's activity permission
      * @return 0 for success, or error code as int < 0*/
     int setGroupLocationPermission(String groupName, boolean permission);
 
-    /*removes all groups with that name
-   * @param String phoneNumber
-   * @return # of contacts removed as int >= 0, or error code as int < 0*/
+    /**removes all groups with that name
+     * @return # of contacts removed as int >= 0, or error code as int < 0*/
     int setGroupActivityPermission(String groupName, boolean permission);
 
-    /*use to get group info from a group name
-   * @param String groupName
-   * @return group if found, null if not found or there was an error*/
+    /**use to get group info from a group name
+     * @return group if found, null if not found or there was an error*/
     Group getGroupInfo(String groupName);
 
-    /*returns sorted A - Z by name
+    /**returns sorted A - Z by name
   * @return contact if found, null if not found or there was an error*/
     ArrayList<Group> getGroupList();
 
@@ -183,21 +186,17 @@ public interface DBInstance {
     /////////////////////////////////
 
     /*adds new entry to the developer log
-  * @param Date timeStamp, String entry*/
-    //void addDevLog(Date timeStamp, String entry);
+      //void addDevLog(Date timeStamp, String entry);
 
     /*returns log at that index
-  * @param int index
-  * @return log if found, null if not found or there was an error*/
+    * @return log if found, null if not found or there was an error*/
     //DeveloperLog getDevLog(int index);
 
     /*returns all logs between first and last index
-     * @param int first, int last
      * @return log range if found, null if not found or there was an error*/
     //ArrayList<DeveloperLog> getDevLogRange(int first, int last);
 
     /*returns all logs between first and last date
-  * @param Date start, Date end
-  * @return log range if found, null if not found or there was an error*/
+    * @return log range if found, null if not found or there was an error*/
     //ArrayList<DeveloperLog> getDevLogRangeByDate(Date start, Date end);
 }
